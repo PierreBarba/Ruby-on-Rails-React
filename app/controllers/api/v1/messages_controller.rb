fe5a66e8ed5bbbdecbd9be2f3381361f3f81aa02
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-05-09 17:43:54 -0600

    check all sample

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
     def index
-        @messages = Message.all
-        render json: @messages.sample
+        @message = Message.all.sample
+        render json: @message
     end
 end
